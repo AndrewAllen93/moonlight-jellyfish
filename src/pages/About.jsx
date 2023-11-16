da18da4b2336adf29fae530488dd07687093b797
Andrew Allen <allenkandrew@gmail.com>
2023-11-16 15:18:36 -0500

    fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,9 +10,9 @@ function AboutMe() {
         textAlign: "center",
       }}
     >
-      <img class="photo" src="../src/images/IMG_0198.JPG" alt="" />
-      <h2 style={{ fontSize: "30px", paddingBottom: "10px" }}></h2>
+      <img className="photo" src="/images/IMG_0198.JPG" alt="" />
       <div>
+        <h2 style={{ fontSize: "30px", paddingBottom: "10px" }}></h2>
         <p>
           Hello! I'm Andrew, a dedicated husband and father of two wonderful
           girls. Family is at the center of my world, and I cherish the joy and
