7ae00713a4b69ed836808b750f3c1d7c97319f36
Andrew Allen <allenkandrew@gmail.com>
2023-11-16 07:09:15 -0500

    removed name that was used twice on home

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
       }}
     >
       <img class="photo" src="../src/images/IMG_0198.JPG" alt="" />
-      <h2 style={{ fontSize: "30px", paddingBottom: "10px" }}>Andrew Allen</h2>
+      <h2 style={{ fontSize: "30px", paddingBottom: "10px" }}></h2>
       <div>
         <p>
           Hello! I'm Andrew, a dedicated husband and father of two wonderful
