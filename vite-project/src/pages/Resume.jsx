89591dfe260564b8eab3c08ff90024d8d4b2ff68
Andrew Allen <allenkandrew@gmail.com>
2023-11-16 01:53:12 -0500

    added downloadble resume

--- vite-project/src/pages/Resume.jsx
+++ vite-project/src/pages/Resume.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-
 export default function Resume() {
   return (
     <div style={{ fontFamily: "Poppins", color: "#fff", padding: "20px" }}>
       <h1>Resume</h1>
 
-      
       <section>
         <h2>Personal Information</h2>
         <p>Name: Andrew Allen</p>
@@ -15,57 +13,58 @@ export default function Resume() {
         <p>Address: 2006 Planters Knoll Dr, Monroe NC 28110</p>
       </section>
 
-      
       <section>
         <h2>Education</h2>
         <p>
-          Highschool Diploma from Andrew Jackson Highschool
+          High school Diploma from Andrew Jackson High school
           <br />
           Full-Stack Coding Certificate from UNC Charlotte Bootcamp
         </p>
       </section>
 
-      
       <section>
         <h2>Work Experience</h2>
         <p>
-          Assisstant General Manager
+          Assistant General Manager
           <br />
           Sherwin Williams Paint Company, 2022-2023
           <br />
           Atria Senior Living formerly Holiday Retirement, 2016-2021
         </p>
-        
       </section>
 
-      
       <section>
         <h2>Skills</h2>
         <p>
-          JavaScript (React, Node.js)<br />
-          HTML, CSS<br />
+          JavaScript (React, Node.js)
+          <br />
+          HTML, CSS
+          <br />
           Database Management (MySQL, MongoDB)
-         
         </p>
       </section>
 
-      
       <section>
         <h2>Projects</h2>
         <p>
-          ProBlogger Blogging Platform<br />
+          ProBlogger Blogging Platform
+          <br />
           Dirty Shoes Hiking Trail Finder
         </p>
       </section>
 
-      
       <section>
         <h2>Certifications</h2>
         <p>
-          Full-Stack Coding Certificate<br />
+          Full-Stack Coding Certificate
+          <br />
           UNC Charlotte Bootcamp, 2023
         </p>
       </section>
+
+      {/* Link to PDF */}
+      <a href="https://drive.google.com/file/d/1oOYxco8T_d77PflMIDB_i9H9Rp66we_C/view?usp=drive_link" target="_blank">Link Text</a>
+
     </div>
   );
 }
